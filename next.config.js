1e0bd6a264831791a0bd5b7b45645053eac43a15
kyleTres <kyle.tresdin@gmail.com>
2025-08-29 04:12:30 +0800

    fix: Add basePath and assetPrefix for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Remove basePath if deploying to username.github.io or custom domain
-  // basePath: '/hugsy-web',  // Uncomment if deploying to username.github.io/hugsy-web
+  basePath: '/hugsy-website',
+  assetPrefix: '/hugsy-website',
   images: {
     unoptimized: true,
   },
